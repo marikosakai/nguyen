f695a0df06be0f6a13214888c981b2c1ce842f10
Mariko Sakai <marikosakaii@gmail.com>
2014-07-24 18:15:37 +0800

    Add user sign up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
 	def new
 	end
+	
+	def create
+		User.create(name: params[:name], email: params[:email])
+		flash[:notice] = "Your account has been created."
+		redirect_to root_path
+	end
 end
