4672e270cb00b17d3175b36e826dbde7a390fb65
Nguyen Vu Nguyen <nvunguyen@gmail.com>
2014-09-10 19:53:46 +0800

    Sign user in after signing up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,14 @@ class UsersController < ApplicationController
 	end
 	
 	def create
-		User.create(name: params[:name], 
+		user = User.create(name: params[:name], 
 					email: params[:email],
 					password: params[:password],
 					password_confirmation: params[:password_confirmation])
 		flash[:notice] = "Your account has been created."
-		redirect_to root_path
+
+		session[:user_id] = user.id
+		redirect_to user_path(user.id)
 	end
 
 	def show
